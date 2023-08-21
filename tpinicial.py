87faa2ddcb7f1433d0db79876061fa0fc3694021
Ignacio Barrientos <ifbarrientos@outlook.com>
2023-08-21 14:28:38 -0300

    Ajustes para que haya convergencia

--- tpinicial.py
+++ tpinicial.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[49]:
-
 
 import pandas as pd
 import numpy as np
@@ -11,148 +9,73 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 
 
-# In[41]:
-
-
-mydataset = pd.read_csv("../tp inicial/2022-new.csv")
+mydataset = pd.read_csv("./2022-new.csv")
 mydataset
-
-
-# In[ ]:
-
-
-
-
-
-# In[19]:
 
 
 mydataset.keys()
 
 
-# In[21]:
-
-
 mydataset['departamento_id']
 
 
-# In[23]:
-
-
 mydataset[['departamento_nombre', 'cantidad_casos']]
-
-
-# In[66]:
 
 
 mydataset.plot.scatter(x='cantidad_casos', y='provincia_nombre')
 plt.show()
 
 
-# In[33]:
-
-
 mydataset.plot.scatter(x='cantidad_casos', y='departamento_nombre')
 plt.show()
-
-
-# In[44]:
 
 
 mydataset.plot.scatter(x='semanas_epidemiologicas', y='cantidad_casos')
 plt.show()
 
 
-# In[72]:
-
-
 agrupado_semanas = mydataset.groupby('semanas_epidemiologicas')['cantidad_casos'].sum()
-print(agrupado_semanas)
-
-
-# In[70]:
+#print(agrupado_semanas)
 
 
 agrupado_departamento = mydataset.groupby('departamento_nombre')['cantidad_casos'].sum()
-print(agrupado_departamento)
-
-
-# In[71]:
+#print(agrupado_departamento)
 
 
 agrupado_provincia = mydataset.groupby('provincia_nombre')['cantidad_casos'].sum()
-print(agrupado_provincia)
-
-
-# In[69]:
+#print(agrupado_provincia)
 
 
 agrupado_edad = mydataset.groupby('grupo_edad_desc')['cantidad_casos'].sum()
-print(agrupado_edad)
-
-
-# In[77]:
+#print(agrupado_edad)
 
 
 ejemplo = np.array(agrupado_departamento)
-print(ejemplo)
-
-
-# In[75]:
+#print("//////////////////////////////////////////")
+#print(ejemplo)
 
 
 ejemplo2 = mydataset.groupby(['departamento_nombre', 'cantidad_casos'])
-print(ejemplo2)
-
-
-# In[30]:
-
-
+#print(ejemplo2)
 mydataset.head()
-
-
-# In[62]:
-
 
 mydataset = pd.DataFrame(mydataset)
 
 
-# In[31]:
-
-
 x = mydataset.values
-print (x)
+#print("LO DE X ////////////////////////////////////")
+#print(x)
 
 
-# In[67]:
-
-
-
-
-
-# In[88]:
-
-
-departamentos = np.array(mydataset['departamento_id'])
+departamentos = np.array(mydataset['departamento_id']).reshape(-1,1)
+print("LO DE DEPARTAMENTOS ////////////////////////////////////")
 print(departamentos)
 
-
-# In[57]:
-
-
 cantidad = np.array(mydataset['cantidad_casos'])
+print("LO DE CANTIDAD ////////////////////////////////////")
 print(cantidad)
 
-
-# In[82]:
-
-
-regresion_logistica = LogisticRegression()
-regresion_logistica.fit(departamentos,cantidad)
-
-
-# In[ ]:
-
-
-
-
+regresion_logistica = LogisticRegression(solver = 'lbfgs', max_iter=10000)
+regresion_logistica.fit(departamentos, cantidad)
+predicted = regresion_logistica.predict(np.array([720]).reshape(-1,1))
+print(predicted)
