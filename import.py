895ed42cd037d330c85e7e5283c816fc6b167416
Ignacio Barrientos <ifbarrientos@outlook.com>
2023-08-26 19:52:25 -0300

    Subí el archivo con 15000 rows de covid

--- import.py
+++ import.py
@@ -4,6 +4,17 @@ Created on Sat Aug 19 18:02:41 2023
 
 @author: Nacho
 """
-
 import pandas as pd
-mydataset = pd.read_csv('2022-new.csv')
+
+# Load the entire dataset
+# Replace 'your_dataset.csv' with the actual file name and path
+df = pd.read_csv('Covid19Casos.csv')
+
+# Extract the first 15000 rows
+subset_df = df.head(15000)
+
+# Save the extracted subset to a new CSV file
+# Replace 'subset_output.csv' with the desired output file name and path
+subset_df.to_csv('subset_output.csv', index=False)
+
+print("Subset saved to 'subset_output.csv'")
