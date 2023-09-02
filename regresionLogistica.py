5f144635ffb77ead9c2d8398aa624042d54b1efa
Ignacio Barrientos <ifbarrientos@outlook.com>
2023-09-02 20:07:09 -0300

    Cambios a regresionLogistica

--- regresionLogistica.py
+++ regresionLogistica.py
@@ -1,39 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import seaborn as sb
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-def predecir():
-    covid_data = pd.read_csv("./Datos/covid.csv", sep=',')
-
-    covid_data.head()
-
-    covid_data['fecha_diagnostico'] = pd.to_datetime(covid_data['fecha_diagnostico'])
-
-    ##Seleccionamos columnas que vamos a utilizar y quitamos los valores NaN
-    columnas = ['sexo', 'edad', 'residencia_provincia_id', 'clasificacion_resumen', 'fecha_diagnostico']
-    datos_seleccionados = covid_data[columnas].dropna()
-
-    ##Cambiamos a numeros las columas de texto para poder hacer la regresion lineal
-    label_encoder = LabelEncoder()
-    for column in ['sexo', 'clasificacion_resumen', 'fecha_diagnostico']:
-        datos_seleccionados[column] = label_encoder.fit_transform(datos_seleccionados[column])
-        
-    ##guardamos los datos en las variables x e y, x compara todas las columnas excepto calsificacion_resumen
-    X = datos_seleccionados.drop('clasificacion_resumen', axis=1)
-    y = datos_seleccionados['clasificacion_resumen']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    model = LogisticRegression()
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-
-    accuracy = accuracy_score(y_test, y_pred)
-    print(f'Precisión del modelo: {accuracy:.2f}')
+def predecir(edad, sexo):
+    data = pd.read_csv("./Datos/covid.csv", sep=',')
     
-predecir()
+    edad_f = float(edad) #Lo convierto a float porque el dataset tiene las edades en float
+
+    ##Quitamos los valores que no queremos
+    data.drop(data[(data['clasificacion_resumen'] == 'Sospechoso')].index, inplace=True)
+    data.drop(data[(data['sexo'] == 'NR')].index, inplace=True)
+
+    data.dropna(axis=0)
+
+    columnas = ['sexo', 'edad', 'cuidado_intensivo', 'clasificacion_resumen', 'asistencia_respiratoria_mecanica', 'fallecido']
+    datos_seleccionados = data[columnas].dropna()
+    datos_seleccionados['sexo'] = datos_seleccionados['sexo'].str.strip().map({'M': 0, 'F': 1})
+    datos_seleccionados['cuidado_intensivo'] = datos_seleccionados['cuidado_intensivo'].str.strip().map({'NO': 0, 'SI': 1})
+    datos_seleccionados['asistencia_respiratoria_mecanica'] = datos_seleccionados['asistencia_respiratoria_mecanica'].str.strip().map({'NO': 0, 'SI': 1})
+    datos_seleccionados['fallecido'] = datos_seleccionados['fallecido'].str.strip().map({'NO': 0, 'SI': 1})
+    datos_seleccionados['clasificacion_resumen'] = datos_seleccionados['clasificacion_resumen'].str.strip().map({'Descartado': 0, 'Confirmado': 1})
+    
+    datos_seleccionados.drop(datos_seleccionados[(datos_seleccionados['edad'] != edad_f)].index, inplace=True)
+#    datos_seleccionados.drop(datos_seleccionados[(datos_seleccionados['sexo'] != sexo)].index, inplace=True)
+
+    print(datos_seleccionados[(datos_seleccionados['edad'] == edad_f)])
+
+    X = datos_seleccionados.drop('fallecido', axis=1)
+    y = datos_seleccionados['fallecido']
+    X_train, X_validation, y_train, y_validation = train_test_split(X, y, test_size=0.20, random_state=7)
+    model = LogisticRegression(max_iter=15000)
+    model.fit(X_train, y_train)
+    prediction = model.predict(X_validation)
+    accuracy = accuracy_score(y_validation, prediction)
+    return accuracy
+
+print(predecir(55,0))
